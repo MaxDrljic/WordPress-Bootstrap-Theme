f04111644f65e33faaa531a0d9e23fbf2c20f1a1
MaxDrljic <max_spree@hotmail.com>
2018-11-19 09:55:09 +0100

    Added necessary assets

--- header.php
+++ header.php
@@ -17,7 +17,24 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<!-- Bootstrap Core CSS-->
+  <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- FontAwesome Icons -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+    crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+
 	<?php wp_head(); ?>
+
+	<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries-->
+  <!--[if lt IE9]>
+  	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
@@ -42,6 +59,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
+
             <a class="navbar-brand" href="/"><img src="assets/img/logo.png" alt="WordPress Bootstrap"></a>
           </div><!-- navbar-header -->
 
